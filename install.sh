d8291906421c611d1cae6d77c5a0be217caa6b16
Qingming He <906459647@qq.com>
2015-03-08 02:18:08 +0800

    use double brace instead of single brace

--- install.sh
+++ install.sh
@@ -2,17 +2,21 @@
 
 for f in `ls config/`;
 do
-    echo "install config/$f to ~/.$f"
-    if [ -f ~/."$f" ]
+    echo install config/$f to ~/.$f
+    if [[ -f ~/.$f ]]
     then
-        cp ~/."$f" ~/."$f".bak
+        cp ~/.$f ~/.$f.bak
     fi
-    ln -sf "$PWD/config/$f" ~/."$f"
+    ln -sf $PWD/config/$f ~/.$f
 done
 
-echo "install vim configuration"
-if [ -d ~/.vim ]
+echo install vim configuration
+if [[ -d ~/.vim ]]
 then
+    if [[ -d ~/.vim.bak ]]
+    then
+        rm -rf ~/.vim.bak
+    fi
     mv ~/.vim ~/.vim.bak
 fi
 cp -r vim ~/.vim
